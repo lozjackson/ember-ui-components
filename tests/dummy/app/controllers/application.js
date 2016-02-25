74ea6deb440866b028304dccc1a0864799d1d458
Loz Jackson <lozjackson@talktalk.net>
2016-02-25 21:24:11 +0000

    update dummy app

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,23 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-
-  selectBoxValue: null,
-
-  selectBoxOptions: [
-    'Active',
-    'Complete'
-  ],
-
-  selectBoxValue2: null,
-
-  selectBoxOptions2: [
-    {
-      value: 'active-value',
-      text: 'Active'
-    },{
-      value: 'complete-value',
-      text: 'Complete'
-    }
-  ]
 });
