0ff5c87c737edb8fdcd0134529862f5064dfa437
Loz Jackson <lozjackson@talktalk.net>
2016-03-06 22:47:09 +0000

    update dummy app

---
+++ tests/dummy/app/controllers/context-menu-component.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    edit() {
+      // Ember.Logger.log('edit');
+    },
+    delete() {
+      // Ember.Logger.log('delete');
+    }
+  }
+});
