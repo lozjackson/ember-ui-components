b513fff45d0fb76c5a319f0abf477123968dce49
Loz Jackson <lozjackson@talktalk.net>
2016-03-10 21:49:38 +0000

    update context-menu component

--- addon/components/context-menu.js
+++ addon/components/context-menu.js
@@ -3,7 +3,40 @@
 */
 import Ember from 'ember';
 import layout from '../templates/components/context-menu';
-import { getElement, setPosition } from 'ember-ui-components/lib/fn';
+import { getDimensions, getMousePosition, calculatePosition } from 'ember-ui-components/lib/fn';
+
+/*
+  ## setPosition
+
+  Set the left/top css properties of an element.
+
+  `element` should be a reference to an HTML element.  Either a string selector
+  that can be used with jQuery, or a jQuery selection object.
+
+  If `position` is not specified, then the current mouse position will be used.
+
+  `position` should be an Ember.Object with `x` and `y` properties.
+  Both `x` and `y` should be numbers
+
+  method setPosition
+  param {String|Object} element
+  param {Object} position
+*/
+function setPosition(element, position) {
+  let margin = 5;
+  if (typeof element === 'string') {
+    element = Ember.$(element);
+  }
+  if (!position) {
+    position = getMousePosition(window.event);
+  }
+  let scrollBarWidth = (window.innerWidth - $(window).width());
+  let el = getDimensions(element);
+  element.css({
+    'left': calculatePosition( position.get('x') + 2, el.width, window.innerWidth - ( margin + scrollBarWidth)),
+    'top': calculatePosition( position.get('y'), el.height, window.innerHeight - margin)
+  });
+}
 
 /**
   @class ContextMenuComponent
@@ -98,9 +131,8 @@ export default Ember.Component.extend({
     @method didInsertContextMenu
   */
   didInsertContextMenu() {
-    let element = getElement('.context-menu-container');
+    let element = this.$('.context-menu-container');
     setPosition(element);
-    // checkOverflow(element);
   },
 
   /**
