975ba49cb4e65d5ddb884468d50a117f1fd9770a
Loz Jackson <lozjackson@talktalk.net>
2016-03-16 08:00:10 +0000

    update dialog service

--- addon/services/dialog.js
+++ addon/services/dialog.js
@@ -43,10 +43,47 @@ export default Ember.Service.extend({
   deferred: null,
 
   /**
+    ## Open Modal
+
+    Use this method to open a modal dialog.  It takes two arguments.  A `message`
+    and a `type`.  The message can be a string, or an object describing the message.
+    The `type` is the component name to render.
+
+    An example passing in a message string:
+
+    ```
+    dialog.openModal('Something happened!', 'modal-name');
+    ```
+
+    An example passing in an message object:
+
+    ```
+    dialog.openModal({
+      title: 'Foo',
+      body: 'Bar'
+    }, 'modal-name');
+    ```
+
+    An example using the returned Promise:
+
+    ```
+    dialog.openModal({
+      title: 'Are you sure?',
+      body: 'Do you really want to do this?'
+    }, 'modal-name').then(() => {
+
+      // confirmed...
+
+    }, () => {
+
+      // canceled...
+
+    });
+    ```
+
     @method openModal
     @param {Object|String} message
     @param {String} type
-    @private
     @return {Object} Promise
   */
   openModal(message, type) {
@@ -65,6 +102,26 @@ export default Ember.Service.extend({
   },
 
   /**
+    ## Alert
+
+    Use this method to open an alert dialog.  It takes one argument - A `message`.
+    The message can be a string, or an object describing the message.
+
+    An example passing in a message string:
+
+    ```
+    dialog.alert('Something happened!');
+    ```
+
+    An example passing in a message object:
+
+    ```
+    dialog.alert({
+      title: 'An event has happened',
+      body: 'You clicked a button!'
+    });
+    ```
+
     @method alert
     @param {Object|String} message
     @return {Object} Promise
@@ -74,6 +131,40 @@ export default Ember.Service.extend({
   },
 
   /**
+    ## Confirm
+
+    Use this method to open an confirmation dialog.  It takes one argument - A
+    `message`.  The message can be a string, or an object describing the message.
+
+    An example passing in a message string:
+
+    ```
+    dialog.confirm('Are you sure?').then(() => {
+
+      // confirmed...
+
+    }, () => {
+
+      // canceled...
+
+    });
+    ```
+
+    ```
+    dialog.confirm({
+      title: 'Are you sure?',
+      body: 'Do you really want to do this?'
+    }).then(() => {
+
+      // confirmed...
+
+    }, () => {
+
+      // canceled...
+
+    });
+    ```
+
     @method confirm
     @param {Object|String} message
     @return {Object} Promise
