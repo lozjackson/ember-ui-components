5e64ab03db903219abcd2a7bdb3d21e013d7a1d5
Loz Jackson <lozjackson@talktalk.net>
2016-03-17 22:33:17 +0000

    update lookup initializer

--- addon/initializers/lookup.js
+++ addon/initializers/lookup.js
@@ -1,6 +1,6 @@
 export function initialize(/* application */) {
   let application = arguments[1] || arguments[0];
-  application.inject('service:lookup', 'applicationRoute', 'route:application');
+  // application.inject('service:lookup', 'applicationRoute', 'route:application');
   application.inject('service:lookup', 'application', 'controller:application');
 }
 
