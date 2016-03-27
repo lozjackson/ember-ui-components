72a52a90d03222e93e676132b781867dbd0ca124
Loz Jackson <lozjackson@talktalk.net>
2016-03-28 00:17:28 +0100

    add uic-button component

---
+++ addon/components/uic-button.js
@@ -0,0 +1,37 @@
+/**
+  @module ember-ui-components
+*/
+import Ember from 'ember';
+import layout from '../templates/components/uic-button';
+
+/**
+  @class ButtonComponent
+  @namespace Components
+*/
+export default Ember.Component.extend({
+  layout,
+
+  /**
+    @property tagName
+    @type {String}
+    @private
+    @default `button`
+  */
+  tagName: 'button',
+
+  /**
+    @property classNames
+    @type {Array}
+    @private
+    @default `['uic-button']`
+  */
+  classNames: ['uic-button'],
+
+  /**
+    @event click
+    @private
+  */
+  click() {
+    this.sendAction();
+  }
+});
