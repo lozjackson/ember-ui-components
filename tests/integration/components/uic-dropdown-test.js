23fd9f3f3587d17b78559151ee53e48f6fe8cbf3
Loz Jackson <lozjackson@talktalk.net>
2016-03-27 23:40:39 +0100

    update uic-dropdown component

--- tests/integration/components/uic-dropdown-test.js
+++ tests/integration/components/uic-dropdown-test.js
@@ -16,9 +16,39 @@ test('it renders', function(assert) {
   // Template block usage:
   this.render(hbs`
     {{#uic-dropdown}}
+      drop down content
+    {{else}}
       template block text
     {{/uic-dropdown}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
+});
+
+test('it has correct tagName and classNames', function(assert) {
+  this.render(hbs`{{uic-dropdown}}`);
+  assert.equal(this.$('div.uic-dropdown').length, 1);
+});
+
+test('click', function(assert) {
+
+  this.set('showDropdown', false);
+  this.render(hbs`
+    {{#uic-dropdown showDropdown=showDropdown}}
+      <div class="dropdown-content"></div>
+    {{else}}
+      <button>Click</button>
+    {{/uic-dropdown}}
+    `);
+
+  assert.equal(this.get('showDropdown'), false);
+  assert.equal(this.$('div.dropdown-content').length, 0);
+
+  this.$('.uic-dropdown button').click();
+  assert.equal(this.get('showDropdown'), true);
+  assert.equal(this.$('div.dropdown-content').length, 1);
+
+  this.$('.uic-dropdown button').click();
+  assert.equal(this.get('showDropdown'), false);
+  assert.equal(this.$('div.dropdown-content').length, 0);
 });
