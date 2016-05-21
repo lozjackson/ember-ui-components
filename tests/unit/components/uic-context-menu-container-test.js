a03d43148c1db77d01ff715dd7332445c7139ec9
Loz Jackson <lozjackson@talktalk.net>
2016-05-21 19:45:00 +0100

    update context-menu-container component

--- tests/unit/components/uic-context-menu-container-test.js
+++ tests/unit/components/uic-context-menu-container-test.js
@@ -1,4 +1,7 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
+
+const { run } = Ember;
 
 moduleForComponent('uic-context-menu-container', 'Unit | Component | uic context menu container', {
   unit: true
@@ -17,4 +20,62 @@ test('classNames', function(assert) {
   var component = this.subject();
   this.render();
   assert.deepEqual(component.get('classNames'), ['ember-view', 'uic-context-menu-container', 'uic-menu-container']);
+});
+
+test('attributeBindings', function(assert) {
+  assert.expect(2);
+  var component = this.subject();
+  this.render();
+  assert.equal(component.get('attributeBindings').length, 2);
+  assert.equal(component.get('attributeBindings')[1], 'tabindex');
+});
+
+test('classNameBindings', function(assert) {
+  assert.expect(2);
+  var component = this.subject();
+  this.render();
+  assert.equal(component.get('classNameBindings').length, 1);
+  assert.equal(component.get('classNameBindings')[0], 'hideOutline:no-outline');
+});
+
+test('tabindex', function(assert) {
+  assert.expect(1);
+  var component = this.subject();
+  this.render();
+  assert.equal(component.get('tabindex'), 1);
+});
+
+test('hideOutline', function(assert) {
+  assert.expect(1);
+  var component = this.subject();
+  this.render();
+  assert.equal(component.get('hideOutline'), true);
+});
+
+test('keyDown() event - enter', function(assert) {
+  assert.expect(2);
+  let event = {
+    preventDefault: () => assert.ok(false),
+    keyCode: 13
+  };
+  var component = this.subject();
+  // component.set('showContextMenu', true);
+  component.set('action', () => assert.ok(true));
+  this.render();
+  run(() => component.keyDown(event));
+  assert.ok(component);
+});
+
+test('keyDown() event - escape', function(assert) {
+  assert.expect(2);
+  let event = {
+    preventDefault: () => assert.ok(false),
+    keyCode: 27
+  };
+  var component = this.subject();
+  // component.set('showContextMenu', true);
+  component.set('action', () => assert.ok(true));
+  this.render();
+  run(() => component.keyDown(event));
+  assert.ok(component);
 });
