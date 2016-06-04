43e6549ccad84672cdf75f7567f887a8d2edf815
Loz Jackson <lozjackson@talktalk.net>
2016-06-05 00:53:55 +0100

    update open-modal helper

--- addon/helpers/open-modal.js
+++ addon/helpers/open-modal.js
@@ -1,6 +1,58 @@
 import Ember from 'ember';
 
 /**
+  ## OpenModal
+
+  The `{{open-modal}}` helper can be used to open a modal from within templates.
+
+  ### ModalId
+
+  Pass the `id` of the modal to open as the first argument.
+
+  ```
+  {{open-modal "modal-id"}}
+  ```
+
+  The `{{open-modal}}` helper returns a function that can be used in actions:
+  
+  ```
+  <div {{action (open-modal "modal-id")}}>
+    // ...
+  </div>
+  ```
+
+  ```
+  <div click={{open-modal "modal-id"}}>
+    // ...
+  </div>
+  ```
+
+  in a component:
+
+  ```
+  {{my-component action=(open-modal "modal-id")}}
+  ```
+
+  ### Model
+
+  Optionally, you can pass in a model or object as the second argument.  This
+  object will be available as the `modalParams.model` property within the
+  `modal-window` component.
+
+  ```
+  <div {{action (open-modal "modal-id" model)}}>
+    // ...
+  </div>
+
+  {{#uic-modal-window as |params|}}
+    // model is available at params.model
+  {{/uic-modal-window}}
+  ```
+
+  In the example above, the `model` property passed as the second argument to
+  the `{{open-modal}}` helper is available in the `{{uic-modal-window}}`
+  component as the `params.model` property.
+  
   @class OpenModalHelper
   @namespace Helpers
 */
@@ -19,10 +71,10 @@ export default Ember.Helper.extend({
     @private
     @return {Function}
   */
-  compute([menalId]/*, hash*/) {
+  compute([modalId, model]/*, hash*/) {
     let modal = this.get('modal');
     return function (/*event*/) {
-      return modal.open(menalId);
+      return modal.open(modalId, model);
     };
   }
 });
