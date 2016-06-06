7724d4bb7b8f524b556e9a739ba50a066d591049
Loz Jackson <lozjackson@talktalk.net>
2016-06-06 23:05:18 +0100

    update dummy app

--- tests/dummy/app/controllers/modal-window-component.js
+++ tests/dummy/app/controllers/modal-window-component.js
@@ -4,6 +4,10 @@ export default Ember.Controller.extend({
 
   modal: Ember.inject.service(),
 
+  // BEGIN-SNIPPET array-of-items
+  items: ['Foo', 'Bar', 'Baz'],
+  // END-SNIPPET
+
   actions: {
 
     toggle(propertyName) {
