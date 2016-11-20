c48ddf96fe4fc5d999fa0e5c9bb12b2e465fdf3e
Loz Jackson <lozjackson@talktalk.net>
2016-11-20 22:08:55 +0000

    update dummy app

--- tests/dummy/app/controllers/tabs-component.js
+++ tests/dummy/app/controllers/tabs-component.js
@@ -2,12 +2,33 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-// BEGIN-SNIPPET array-of-pojos
-tabs: Ember.A([
-  { name: 'Tab 1', data: 'Tab 1 content' },
-  { name: 'Tab 2', data: 'Tab 2 content' }
-])
-// END-SNIPPET
+  // BEGIN-SNIPPET tab-names
+  tabs: [
+    { tabName: 'First Tab' },
+    { tabName: 'Second Tab' }
+  ],
+  // END-SNIPPET
+
+  // BEGIN-SNIPPET array-of-pojos
+  tabObjects: [
+    { tabName: 'Tab 1', someData: 'Content for tab 1...' },
+    { tabName: 'Tab 2', someData: 'Some other content for tab 2' }
+  ],
+  // END-SNIPPET
+
+  // BEGIN-SNIPPET array-of-components
+  tabComponents: [
+    { tabName: 'Tab 1', tabComponent: 'my-first-tab' },
+    { tabName: 'Tab 2', tabComponent: 'my-second-tab' }
+  ],
+  // END-SNIPPET
+
+  // BEGIN-SNIPPET array-of-templates
+  tabTemplates: [
+    { tabName: 'Tab 1', tabTemplate: 'my-first-tab' },
+    { tabName: 'Tab 2', tabTemplate: 'my-second-tab' }
+  ]
+  // END-SNIPPET
 
 
 });
