1817c46fc44fcd94e3e6bec884bf28fb9872051c
Loz Jackson <lozjackson@talktalk.net>
2017-01-01 16:32:51 +0000

    update docs

--- addon/components/uic-list.js
+++ addon/components/uic-list.js
@@ -1,8 +1,40 @@
+/**
+  @module ember-ui-components
+*/
 import Ember from 'ember';
 import layout from '../templates/components/uic-list';
 
+/**
+  ## List Component
+
+  ```hbs
+  {{uic-list model=array}}
+  ```
+
+  ```hbs
+  {{#uic-list model=array as |item|}}
+    ...
+  {{/uic-list}}
+  ```
+
+  @class ListComponent
+  @namespace Components
+*/
 export default Ember.Component.extend({
   layout,
+
+  /**
+    @property classNames
+    @type {Array}
+    @default `['ul']`
+  */
   tagName: 'ul',
+
+  /**
+    @property classNames
+    @type {Array}
+    @private
+    @default `['uic-list']`
+  */
   classNames: ['uic-list']
 });
