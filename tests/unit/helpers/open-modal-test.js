1feaa913cea6acc6286ea013f57b884936bef289
Loz Jackson <lozjackson@talktalk.net>
2017-02-07 22:42:21 +0000

    update test

--- tests/unit/helpers/open-modal-test.js
+++ tests/unit/helpers/open-modal-test.js
@@ -3,7 +3,6 @@ import { module, test } from 'qunit';
 
 module('Unit | Helper | open modal');
 
-// Replace this with your real tests.
 test('it works', function(assert) {
   assert.equal(typeof openModal, 'function');
 });
